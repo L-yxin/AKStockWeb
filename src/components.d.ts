87b20ba3663a6262eba28bebb78f7d277246ae58
L-yxin <2768592753@qq.com>
2026-04-23 11:12:05 +0800

    T2

--- src/components.d.ts
+++ src/components.d.ts
@@ -12,6 +12,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     App: typeof import('./App.vue')['default']
+    AppMenu: typeof import('./components/AppMenu.vue')['default']
     ElAside: typeof import('element-plus/es')['ElAside']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
